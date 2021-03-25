1ec2df4432b122eeaeffda38070b42a8a10b48a2
Amie <2472510m@student.gla.ac.uk>
2021-03-25 10:14:15 +0000

    created view functions, mapped urls to views

--- gamewebsite/urls.py
+++ gamewebsite/urls.py
@@ -4,9 +4,11 @@ from gamewebsite import views
 app_name = 'gamewebsite'
 
 urlpatterns = [
-    path('', views.game_library, name = 'game_library'),
+    path('', views .game_library, name = 'game_library'),
     path('contact-us/', views.contact_us, name = 'contact_us'),
     path('login/', views.log_in, name = 'log_in'),
     path('my-account/', views.my_account, name = 'my_account'),
     path('my-account/edit/', views.edit_account, name='edit_account'),
+    # path('GameLibrary/<slug:game_name_slug>/', views.game_page, name='game_page')
+    # path('GameLibrary/<slug:game_name_slug>/search-matches/', views.search_matches, name='search_matches'),
     ]
--- gamewebsite/views.py
+++ gamewebsite/views.py
@@ -1,20 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from gamewebsite.models import Game
 
+# displays games the user can choose from
 def game_library(request):
-    return render(request, 'gamewebsite/base.html')
+#    context_dict = {}
+    # return 4 most popular in descending order
+    
+    # return 4 best rated in descending order
+    
+    # return 4 newest in descending order
+#    newest_games = Category.objetcs.order_by('-date_added')[:4]
+
+#    context_dict['newest']= newest_games
+    
+    return render(request, 'gamewebsite/base.html') #, context= context_dict)
  
-def search_matches(request):
-    return HttpResponse("search for matches")
+# the user can search for other users that fit their requirements
+# def search_matches(request):
+    # return render(request, 'gamewebsite/search_matches.html')
 
+# the user can log in to their account
 def log_in(request):
-    return HttpResponse("log in")
+    return render(request, 'gamewebsite/log_in.html')
 
+# the user can view their account details
 def my_account(request):
-    return HttpResponse("my account")
+    return render(request, "gamewebsite/my_account.html")
 
+# the user can edit their account
 def edit_account(request):
-    return HttpResponse("edit account")
-
+    return render(request, "gamewebsite/edit_account.html")
+    
+# displays information about a user selected game
+# def game_page(request, game_name_slug):
+    
+    # return HttpResponse('game page')
+    
 def contact_us(request):
-    return HttpResponse("contact us")
+    return render(request, "gamewebsite/contact_us.html")
+
