d471fee414b97b2391ba64ec4243484c8d36d14d
Amie <2472510m@student.gla.ac.uk>
2021-03-30 14:47:12 +0100

    game page template updated to show game name and description, added GameAdmin class in admin.py

--- gamewebsite/admin.py
+++ gamewebsite/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from gamewebsite.models import *
 # Register your models here.
+
+class GameAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+    
+admin.site.register(TimeZones)
+admin.site.register(Languages)
+admin.site.register(UserProfile)
+admin.site.register(MatchRequests)
+admin.site.register(AcceptedMatches)
+admin.site.register(Game, GameAdmin)
