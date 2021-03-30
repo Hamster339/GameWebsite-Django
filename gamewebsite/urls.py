9cf61dda932d215c51d2a4f16fe586aff12a2ec0
Amie <2472510m@student.gla.ac.uk>
2021-03-30 14:49:53 +0100

    url added to map a game to the game page template

--- gamewebsite/urls.py
+++ gamewebsite/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('my-account/', views.my_account, name = 'my_account'),
     path('signup/', views.sign_up, name = 'sign_up'),
     #path('my-account/edit/', views.edit_account, name='edit_account'),
-    # path('GameLibrary/<slug:game_name_slug>/', views.game_page, name='game_page')
+    path('GameLibrary/<slug:game_name_slug>/', views.game_page, name='game_page')
     # path('GameLibrary/<slug:game_name_slug>/search-matches/', views.search_matches, name='search_matches'),
     ]
