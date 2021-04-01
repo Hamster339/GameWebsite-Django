e18996ce65e9ec532ce618bb82be994317f86b0e
Hamish Philip <2464927p@student.gla.ac.uk>
2021-04-01 14:19:42 +0100

    Loging out link added to base template

--- gamewebsite/urls.py
+++ gamewebsite/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('login/', views.log_in, name = 'log_in'),
     path('my-account/', views.my_account, name = 'my_account'),
     path('signup/', views.sign_up, name = 'sign_up'),
+    path('logout/', views.user_logout, name = 'log_out'),
     #path('my-account/edit/', views.edit_account, name='edit_account'),
     path('GameLibrary/<slug:game_name_slug>/', views.game_page, name='game_page')
     # path('GameLibrary/<slug:game_name_slug>/search-matches/', views.search_matches, name='search_matches'),
