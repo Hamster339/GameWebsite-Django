747c1f41bda46722330f514c1d85efea600e69f1
Hamish Philip <2464927p@student.gla.ac.uk>
2021-04-02 16:50:57 +0100

    removed duplicate gamelibary url. game libary is now only homepage ([ip_address]/...)

--- gamewebsite/urls.py
+++ gamewebsite/urls.py
@@ -5,7 +5,6 @@ app_name = 'gamewebsite'
 
 urlpatterns = [
     path('', views .game_library, name = 'game_library'),
-    path('Game-Library/', views.game_library, name = 'game_library'),
     path('search/', views.search, name = 'search'),
     path('contact-us/', views.contact_us, name = 'contact_us'),
     path('login/', views.log_in, name = 'log_in'),
@@ -13,6 +12,6 @@ urlpatterns = [
     path('signup/', views.sign_up, name = 'sign_up'),
     path('logout/', views.user_logout, name = 'log_out'),
     #path('my-account/edit/', views.edit_account, name='edit_account'),
-    path('Game-Library/<slug:game_name_slug>/', views.game_page, name='game_page')
+    path('<slug:game_name_slug>/', views.game_page, name='game_page')
     # path('GameLibrary/<slug:game_name_slug>/search-matches/', views.search_matches, name='search_matches'),
     ]
